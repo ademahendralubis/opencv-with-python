66e1c51672ddb83cd24e7f549e627124f1db4d04
codingan <codinganai@gmail.com>
2020-08-19 23:31:03 +0700

    add comment

--- 4. resizing image/code.py
+++ 4. resizing image/code.py
@@ -5,13 +5,19 @@ import cv2
 #load image
 image = cv2.imread('image.png')
 
+#print the shape of image
 print(image.shape)
 
+#resizing image (width,height)
 resize = cv2.resize(image, (200,200))
 
+#show original image
 cv2.imshow('original', image)
+
+#show resized image
 cv2.imshow('resized', resize)
 if cv2.waitKey(0) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
 
+#save the resized image
 cv2.imwrite("resize.png",resize)
