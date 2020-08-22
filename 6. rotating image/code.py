4ad1fc1e22a6decbe116da511fa3090319279859
codingan <codinganai@gmail.com>
2020-08-22 23:10:50 +0700

    add some comment

--- 6. rotating image/code.py
+++ 6. rotating image/code.py
@@ -5,10 +5,19 @@ import cv2
 #load image
 image = cv2.imread('image.png')
 print(image.shape)
+
+#extract the height and width of the image
 h, w, _ = image.shape
 
+#center of the image
 center = (w/2, h/2)
+
+#set rotation (center coordinate, angles, scale)
 rotation = cv2.getRotationMatrix2D(center, -10, -0.1)
+
+#rotate image (source, rotation setting, (width, height))
 rotated_img = cv2.warpAffine(image, rotation, (w,h))
+
+#show image
 cv2.imshow("rotated img", rotated_img)
 cv2.waitKey(0)
