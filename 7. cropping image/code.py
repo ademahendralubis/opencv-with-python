13b8f2a54261f2db5a579ee52eee6c375a818b1e
codingan <codinganai@gmail.com>
2020-08-23 22:52:04 +0700

    update file

--- 7. cropping image/code.py
+++ 7. cropping image/code.py
@@ -6,8 +6,10 @@ import cv2
 image = cv2.imread('image.png')
 print(image.shape)
 
+#cropping image
 crop = image[130:230, 200:270]
 
+#show image
 cv2.imshow("full", image)
 cv2.imshow('crop',crop)
 cv2.waitKey(0)
