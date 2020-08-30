732bdaae8ed21e61a807d9a1df0ff5974550fc4f
codingan <codinganai@gmail.com>
2020-08-30 23:34:24 +0700

    plotting histograms

---
+++ 14. Plotting histograms/code.py
@@ -0,0 +1,15 @@
+import cv2
+import matplotlib.pyplot as plt
+
+#Show image
+image = cv2.imread('image2.jpg',cv2.IMREAD_GRAYSCALE)
+
+#Show image
+cv2.imshow("image", image)
+
+#Show histogram
+plt.hist(image.ravel(), 256, [0,256])
+plt.show()
+
+
+cv2.waitKey(0)
